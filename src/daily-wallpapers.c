620180d7d2abe7ded51dc2c55152d76c79200b12
Gustavo Mantovani <mantovanig000@gmail.com>
2024-02-12 22:44:43 -0300

    Translating the source code to english

--- src/daily-wallpapers.c
+++ src/daily-wallpapers.c
@@ -5,8 +5,8 @@
 #include <string.h>
 #include <dirent.h>
 
-FILE * abriRegistroDw(const char *modo){
-  FILE *arq;
+FILE * openDwRecord(const char *mode){
+  FILE *file;
   char *home = getenv("HOME");
   char *record_path;
 
@@ -15,67 +15,68 @@ FILE * abriRegistroDw(const char *modo){
   strcpy(record_path,home);
   strcat(record_path,"/.dw/curr_wpp");
   
-  arq = fopen(record_path, modo);
+  file = fopen(record_path, mode);
 
   free(record_path);
-  return arq;
+  return file;
 }
 
-void lerWallpaperAtual(char *wppAtual){
-  FILE *arq;
-  arq=abriRegistroDw("r");
-  if(arq==NULL){
-    strcpy(wppAtual, "\0");
+void readCurrentWallpaper(char *currentWallpaper){
+  FILE *file;
+  file = openDwRecord("r");
+  if(file == NULL){
+    strcpy(currentWallpaper, "\0");
     return;
-  }else{
-    if(fgets(wppAtual,PATH_MAX,arq)==NULL) strcpy(wppAtual, "\0"); 
-    fclose(arq);
+  } else {
+    if(fgets(currentWallpaper, PATH_MAX, file) == NULL) 
+      strcpy(currentWallpaper, "\0"); 
+    fclose(file);
   }  
 }
 
-void trocarWallpaper(char *wppPath){
+void changeWallpaper(char *wallpaperPath){
   char cmd[ARG_MAX];
 
-  //modo dark
+  // dark mode
   strcpy(cmd, "gsettings set org.gnome.desktop.background picture-uri-dark ");
-  strcat(cmd, wppPath);
+  strcat(cmd, wallpaperPath);
   system(cmd);
 
-  //modo claro
+  // light mode
   strcpy(cmd, "gsettings set org.gnome.desktop.background picture-uri ");
-  strcat(cmd, wppPath);
+  strcat(cmd, wallpaperPath);
   system(cmd);
 }
 
-void setarPrimeiroCandidatoWpp(char *path){
+void setFirstCandidateWallpaper(char *path){
   DIR *dir = opendir(path);
-  char pathMoreDir[PATH_MAX],*mimeTypeOnly;
+  char pathMoreDir[PATH_MAX], *mimeTypeOnly;
   magic_t magic_cookie;
-  FILE *arq;
+  FILE *file;
 
-  struct dirent *entrada;
+  struct dirent *entry;
   magic_cookie = magic_open(MAGIC_MIME_TYPE);
   magic_load(magic_cookie, NULL);
 
-  while ((entrada = readdir(dir)) != NULL) {
-    if(entrada->d_type==8){
+  while ((entry = readdir(dir)) != NULL) {
+    if(entry->d_type == 8){
 
       strcpy(pathMoreDir, path);
-      strcat(pathMoreDir, entrada->d_name);
+      strcat(pathMoreDir, entry->d_name);
       mimeTypeOnly = strtok(magic_file(magic_cookie, pathMoreDir),"/");
 
       if(!strcmp(mimeTypeOnly, "image")){
-        trocarWallpaper(pathMoreDir);
+        changeWallpaper(pathMoreDir);
 
-        arq = abriRegistroDw("w");
-        if(arq==NULL){
-          system("notify-send -t 3000 'Daily Wallpaper' 'Não foi possível acessar o path do wallpaper atual'");
+        file = openDwRecord("w");
+        if(file == NULL){
+          system("notify-send -t 3000 'Daily Wallpaper' 'Could not access the current wallpaper path'");
           exit(-1);
         }
 
-        fprintf(arq, "%s", pathMoreDir);
+        fprintf(file, "%s", pathMoreDir);
 
-        fclose(arq);
+        fclose(file);
         magic_close(magic_cookie);
         closedir(dir);
         return;
@@ -86,78 +87,78 @@ void setarPrimeiroCandidatoWpp(char *path){
   closedir(dir);
 }
 
-void setarProximoCandidatoWpp(char *path, char *pathWppAtual){
+void setNextCandidateWallpaper(char *path, char *currentWallpaperPath){
   DIR *dir = opendir(path);
-  char pathMoreDir[PATH_MAX],*mimeTypeOnly;
+  char pathMoreDir[PATH_MAX], *mimeTypeOnly;
   magic_t magic_cookie;
-  FILE *arq;
+  FILE *file;
 
-  struct dirent *entrada;
+  struct dirent *entry;
   magic_cookie = magic_open(MAGIC_MIME_TYPE);
   magic_load(magic_cookie, NULL);
 
-  while ((entrada = readdir(dir)) != NULL) {
-    if(entrada->d_type==8){
+  while ((entry = readdir(dir)) != NULL) {
+    if(entry->d_type == 8){
 
       strcpy(pathMoreDir, path);
-      strcat(pathMoreDir, entrada->d_name);
+      strcat(pathMoreDir, entry->d_name);
       mimeTypeOnly = strtok(magic_file(magic_cookie, pathMoreDir),"/");
 
-      if(!strcmp(pathMoreDir, pathWppAtual) && !strcmp(mimeTypeOnly, "image")){
+      if(!strcmp(pathMoreDir, currentWallpaperPath) && !strcmp(mimeTypeOnly, "image")){
         
-        //ler entradas até o próximo mime type image, então setar essa entrada
-        while((entrada = readdir(dir))!=NULL){
-          if(entrada->d_type==8){
+        // read entries until the next image mime type, then set that entry
+        while((entry = readdir(dir)) != NULL){
+          if(entry->d_type == 8){
             strcpy(pathMoreDir, path);
-            strcat(pathMoreDir, entrada->d_name);
+            strcat(pathMoreDir, entry->d_name);
             mimeTypeOnly = strtok(magic_file(magic_cookie, pathMoreDir),"/");
 
             if(!strcmp(mimeTypeOnly, "image")){
-              trocarWallpaper(pathMoreDir);
+              changeWallpaper(pathMoreDir);
              
-              arq = abriRegistroDw("w");
-              if(arq==NULL){
-                system("notify-send -t 3000 'Daily Wallpaper' 'Não foi possível acessar o path do wallpaper atual'");
+              file = openDwRecord("w");
+              if(file == NULL){
+                system("notify-send -t 3000 'Daily Wallpaper' 'Could not access the current wallpaper path'");
                 exit(-1);
               }
 
-              fprintf(arq, "%s", pathMoreDir);
+              fprintf(file, "%s", pathMoreDir);
 
               magic_close(magic_cookie);
               closedir(dir);
-              fclose(arq);
+              fclose(file);
               return;
             } 
           }
         }
-        setarPrimeiroCandidatoWpp(path);
+        setFirstCandidateWallpaper(path);
         magic_close(magic_cookie);
         closedir(dir);
         return;
       }
     }   
   }
-  setarPrimeiroCandidatoWpp(path);
+  setFirstCandidateWallpaper(path);
   magic_close(magic_cookie);
   closedir(dir);
 }
 
 int main(){
-  char wallpaperAtual[PATH_MAX];
+  char currentWallpaper[PATH_MAX];
   char path[PATH_MAX];
 
-  //stdin
-  fgets(path,PATH_MAX,stdin);
-  path[strlen(path)-1]='\0';
+  // stdin
+  fgets(path, PATH_MAX, stdin);
+  path[strlen(path)-1] = '\0';
 
-  lerWallpaperAtual(wallpaperAtual);
+  readCurrentWallpaper(currentWallpaper);
 
-  if(strlen(wallpaperAtual)==0){
-    //setar o primeiro candidato como wallpaper
-    setarPrimeiroCandidatoWpp(path);
-  }else{
-    //buscar na lista e setar o próximo (caso seja o primeiro ou não esteja mais na lista, setar o primeiro)
-    setarProximoCandidatoWpp(path, wallpaperAtual);
+  if(strlen(currentWallpaper) == 0){
+    // set the first candidate as wallpaper
+    setFirstCandidateWallpaper(path);
+  } else {
+    // search the list and set the next (if it's the first or no longer in the list, set the first)
+    setNextCandidateWallpaper(path, currentWallpaper);
   }
 
   return 0;
